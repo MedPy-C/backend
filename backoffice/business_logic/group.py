05d13d9d4fce6afa096346e6d0c69a9c3887d0f1
chrismedinapy <chrismedinapy@gmail.com>
2021-05-21 22:34:09 -0400

    added methods for list all groups, and a more detailed data from one single group

--- backoffice/business_logic/group.py
+++ backoffice/business_logic/group.py
@@ -6,10 +6,21 @@ from backoffice.utils.exceptions import EntityNotFound
 
 
 class GroupLogic():
+    """
+    Group bussines logic.
+    Methods for create, list, retrieve, update, and delete group model
+    """
+
     def __init__(self):
-        self.fields = ['membership_code', 'group']
+        self.fields = ['name', 'slug_name', 'about']
 
     def create(self, user_login_code, group_data):
+        """
+        create function of group model
+        :param user_login_code: uuid
+        :param group_data: serialized data of group
+        :return: if no errors present does not raise exceptions.
+        """
         user = models.UserLogin.objects.get_user_by_code(user_login_code)
         if not user:
             raise EntityNotFound(
@@ -31,19 +42,61 @@ class GroupLogic():
         saved_membership = models.Membership.objects.save(membership_model)
 
     def list(self, user_login_code):
+        """
+        list user group by user code
+        :param user_login_code: uuid
+        :return: a list of dict with data of the group that the user it is part of.
+        """
         user = models.UserLogin.objects.get_user_by_code(user_login_code)
         if not user:
             raise EntityNotFound(
                 f'User with code {user_login_code} not found.'
             )
-        # groups = models.Group.objects.get_all_groups(user_login_code)
-        groups = models.Membership.objects.get_all_memberships(user_login_code)
+        groups = models.Group.objects.get_all_groups(user_login_code)
+        # groups = models.Membership.objects.get_all_memberships(user_login_code)
         group_list = []
         for group in groups:
             group_list.append(self.__mapped_group(group))
         return group_list
 
-    def __mapped_group(self, membership):
-        group_dict = model_to_dict(membership, fields=self.fields)
-        group_dict['membership_code'] = str(membership.membership_code)
+    def retrieve(self, user_login_code, group_code):
+        """
+        retrive only one group,
+        :param user_login_code: uuid
+        :param group_code: uuid
+        :return: a dict with data of the selected group.
+        """
+        user = models.UserLogin.objects.get_user_by_code(user_login_code)
+        if not user:
+            raise EntityNotFound(
+                f'User with code {user_login_code} not found.'
+            )
+        group = models.Group.objects.get_group_by_group_code(user_login_code, group_code)
+        if not group:
+            raise EntityNotFound(
+                f'Group with code {group_code} not found.'
+            )
+        return self.__mapped_group_detailed(group)
+
+    def __mapped_group_detailed(self, group):
+        """
+        we map from group object to a dict with data of group
+        :param group: python class object
+        :return: dict with more detailed data of group.
+        """
+        group_dict = model_to_dict(group, fields=self.fields)
+        group_dict['group_code'] = str(group.group_code)
+        group_dict['created_at'] = str(group.created_at)
+        group_dict['is_verified'] = str(group.is_verified)
+        group_dict['is_public'] = str(group.is_public)
+        return group_dict
+
+    def __mapped_group(self, group):
+        """
+        we map from group object to a dict with data of group
+        :param group: python class object
+        :return: dict with data of group.
+        """
+        group_dict = model_to_dict(group, fields=self.fields)
+        group_dict['group_code'] = str(group.group_code)
         return group_dict
