1e2c098f5249d63a31b7e34e3a77b3210ff2e40f
chrismedinapy <chrismedinapy@gmail.com>
2021-05-25 23:57:40 -0400

    check if the logged user corresponds to the endpoint user code

--- middleware/security/permission.py
+++ middleware/security/permission.py
@@ -8,7 +8,6 @@ class Mediapp_bePermission(BasePermission):
     def has_permission(self, request, view):
         if not request.user:
             return False
-
         user_login_code = request.user.get('user_login_code')
         user_login = models.UserLogin.objects.get_user_by_code(user_login_code)
         return bool(user_login)
