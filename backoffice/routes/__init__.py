487b1bfeb82cdc7e651cac095a68a2801dd39d73
chrismedinapy <chrismedinapy@gmail.com>
2021-05-25 22:17:55 -0400

    added new endpoint for group members list

--- backoffice/routes/__init__.py
+++ backoffice/routes/__init__.py
@@ -2,7 +2,8 @@ from .login import login_routes
 from .user_login import user_login_routes
 from .group import group_routes
 from .invitation import invitation_routes
+from .membership import members_routes
 
 routes_backoffice = (
-        login_routes + user_login_routes + group_routes + invitation_routes
+        login_routes + user_login_routes + group_routes + invitation_routes + members_routes
 )
